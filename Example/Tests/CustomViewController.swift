a9d6a5ca69849fe58252ed9eb4af098ce10b3555
Martin Wildfeuer <mwfire@mwfire.de>
2016-07-18 13:15:17 +0200

    Removed comments

--- Example/Tests/CustomViewController.swift
+++ Example/Tests/CustomViewController.swift
@@ -1,11 +1,3 @@
-//
-//  CustomViewController.swift
-//  PopupDialog
-//
-//  Created by Martin Wildfeuer on 18.07.16.
-//  Copyright © 2016 CocoaPods. All rights reserved.
-//
-
 import UIKit
 
 class CustomViewController: UIViewController {
