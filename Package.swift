b0a4161a31621b65bfdbd2c8b2c1eef8db702c9d
Boram Jeong <bbiguduk@gmail.com>
2021-01-27 00:35:39 +0900

    Swift Package Manager support

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PopupDialog",
-            dependencies: [],
+            dependencies: ["DynamicBlurView"],
             path: "PopupDialog/Classes"),
         .testTarget(
             name: "PopupDialogTests",
